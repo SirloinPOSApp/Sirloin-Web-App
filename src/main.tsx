2bbd1544810eb2ab950defb0aeab108f0cca4dcf
deradhista <deradhista@gmail.com>
2023-02-01 20:33:02 +0700

    feat: setting routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./routes/Index";
 import "./styles/index.css";
+import { ProSidebarProvider } from "react-pro-sidebar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <ProSidebarProvider>
     <App />
-  </React.StrictMode>
+  </ProSidebarProvider>
 );
