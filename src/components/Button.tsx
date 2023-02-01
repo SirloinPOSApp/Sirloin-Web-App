439f68e84a5fe8be82b6e7e2ff6e63202f4d4c98
deradhista <deradhista@gmail.com>
2023-02-01 20:30:04 +0700

    Add: components Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { ButtonHTMLAttributes, FC } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label?: string;
+  buttonSet?: string;
+}
+
+const Button: FC<ButtonProps> = ({ label, buttonSet, ...props }) => {
+  return (
+    <button className={`btn tracking-wider ${buttonSet}`} {...props}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
