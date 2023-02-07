94004fe8bcf6467360ab211e00204ad8a3b3acbf
deradhista <deradhista@gmail.com>
2023-02-07 12:34:09 +0700

    style: styling card product

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -23,12 +23,20 @@ export const CardEtalase: FC<CardProps> = ({
 }) => {
   return (
     <div className="card shadow-lg h-96 bg-white" id={`${id}`}>
-      <figure>
-        <img src={`${product_image}`} alt="product" className="w-64 p-5" />
+      <figure className="m-3">
+        <img
+          src={
+            product_image
+              ? product_image
+              : "https://i.pinimg.com/564x/2c/4b/7f/2c4b7f4b0cb5ae1f3879ec36eb64386b.jpg"
+          }
+          alt="product"
+          className="w-64"
+        />
       </figure>
       <div className="card-normal mx-5 my-3">
         <h2 className="card-title text-[#4AA3BA] font-bold">{product_name}</h2>
-        <p className="text-sm">{stock}</p>
+        <p className="text-sm">{`Stok: ${stock}`}</p>
         <p className="font-bold text-lg py-2">{`Rp.${price}`}</p>
       </div>
       <div className="card-actions m-5 justify-end">
