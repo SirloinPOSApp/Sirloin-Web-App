eb0bab8ab5e70b8dc071d37c57a965b7344c7ac8
deradhista <deradhista@gmail.com>
2023-02-09 15:22:23 +0700

    feat: clean code

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,4 @@
-import React, { FC } from "react";
-import { FiShoppingCart } from "react-icons/fi";
-import { useNavigate } from "react-router-dom";
-import Product from "../pages/products/Product";
+import { FC } from "react";
 import Button from "./Button";
 
 interface CardProps {
